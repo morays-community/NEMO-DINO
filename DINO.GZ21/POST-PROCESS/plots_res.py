59dd62b8726f1459ebe372fb258855e6c1cb7717
Alexis Barge <alexis.barge@proton.me>
2024-05-23 12:32:48 +0200

    plot uf and vf

--- DINO.GZ21/POST-PROCESS/plots_res.py
+++ DINO.GZ21/POST-PROCESS/plots_res.py
@@ -13,28 +13,6 @@ import matplotlib.patches as mpatches
 import matplotlib.colors as colors
 
 
-def main(filepath,filepath_ref,plotpath):
-
-    print(filepath)
-    print(filepath_ref)
-
-    # read files
-    ds = xr.open_dataset(filepath)
-    ds_ref = xr.open_dataset(filepath_ref)
-    lon = ds_ref.nav_lon.values
-    lat = ds_ref.nav_lat.values
-
-    # get fields
-    mld = getattr(ds,"somxl010").values[-1] 
-    mld_ref = getattr(ds_ref,"somxl010").values[-1]
-    diff_mld = mld - mld_ref
-
-    # plot
-    lim = 400.
-    infos = [ 'MLD-mle - MLD (m)' , cmocean.cm.balance , colors.Normalize(vmin=-lim, vmax=lim), lambda x: x ]
-    make_plot(diff_mld,lon,lat,infos,plotpath)
-
-
 def make_plot(data,lon,lat,infos,output):
     # args
     title, cmap, norm, tfs = infos
@@ -43,7 +21,7 @@ def make_plot(data,lon,lat,infos,output):
     plt.figure(figsize=(12, 8))
     ax = plt.axes(projection=ccrs.EqualEarth())
     ax.add_feature(cfeature.LAND, zorder=100, edgecolor='k')
-    # colo map
+    # color map
     pcm = ax.pcolormesh(lon, lat, data, cmap=cmap, norm=norm, transform=ccrs.PlateCarree())
     cbar = plt.colorbar(pcm, ax=ax, orientation='vertical', pad=0.05, shrink=0.5)
     plt.title(title)
@@ -52,48 +30,30 @@ def make_plot(data,lon,lat,infos,output):
     plt.close()
 
 
+def main(filepath, var_name, infos):
+
+    print(f'Plotting {var_name}')
+
+    # read files
+    ds = xr.open_dataset(filepath)
+    lon = ds.nav_lon.values
+    lat = ds.nav_lat.values
+
+    # get fields
+    var_val = getattr(ds,var_name).values[-1]
+
+    # plot
+    plotpath = 'NEVERWORLD_' + var_name + '.png'
+    make_plot(var_val,lon,lat,infos,plotpath)
+
+
+
 if __name__=="__main__":
 
-    # args
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-cref', dest='confcase_ref', type=str, default='noconf')
-    parser.add_argument('-c', dest='confcase', type=str, default='noconf')
-    parser.add_argument('-f', dest='freq', type=str, default='1d')
-    parser.add_argument('-y', dest='year', type=str, default=None)
-    parser.add_argument('-m', dest='month', type=str, default=None)
-    args = parser.parse_args()
-    confcase_ref = args.confcase_ref
-    confcase = args.confcase
-    freq = args.freq
-    year = args.year
-    month = "{:02d}".format(int(args.month))
+    # uf
+    infos = [ 'uf (m/s)' , cmocean.cm.balance , colors.Normalize(vmin=-10, vmax=10), lambda x: x ]
+    main( filepath='NEVERWORLD_gridUsurf.nc' , var_name='ext_uf' , infos=infos)
 
-    # guess CONFCASE if not given
-    if confcase == 'noconf':
-        cur_dir = os.getcwd()
-        cur_dir = cur_dir.split(os.path.sep)
-        
-        if cur_dir[-1] != 'CDF' and cur_dir[-2] != 'CTL':
-            # we are not in CDF/CTL dir and confcase not given, impossible to guess
-            print('ERROR: must be executed in the CONFCASE CTL directory')
-            raise SystemExit
-        else:
-            confcase = cur_dir[-3]
-
-    # path to MEAN
-    SDIR = os.environ['SDIR']
-    print(f'Storage dir: {SDIR}')
-    print(f'CONFCASE : {confcase}')
-    conf, case = confcase.split('-')
-    respath = SDIR + '/' + conf + '/' + confcase + '-MEAN'
-    respath_ref = SDIR + '/' + conf + '/' + confcase_ref + '-MEAN'
-    if not os.path.exists(respath) and not os.path.isdir(respath):
-        print(f'ERROR: {respath} does not exist')
-        sys.exit()
-
-    # get files path
-    filepath = respath + "/" + freq + "/" + year + "/" + confcase + "_y" + year + "m" + month + "." + freq + "_gridTsurf.nc"
-    filepath_ref = respath_ref + "/" + freq + "/" + year + "/" + confcase_ref + "_y" + year + "m" + month + "." + freq + "_gridTsurf.nc"
-    plotpath = SDIR + '/' + conf + '/' + confcase + '-PLOTS/' + freq + "/" + year + "/" + confcase + "_y" + year + "m" + month + "." + freq + "_diffmld.png"
-
-    main(filepath,filepath_ref,plotpath)
+    # vf
+    infos = [ 'vf (m/s)' , cmocean.cm.balance , colors.Normalize(vmin=-10, vmax=10), lambda x: x ]
+    main( filepath='NEVERWORLD_gridVsurf.nc' , var_name='ext_vf' )
