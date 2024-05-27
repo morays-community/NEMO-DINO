4d3d0de3d76c83fd372a8f1dab1fc59fb52bfbaf
Etienne Meunier <etiennemeunier@live.fr>
2024-05-27 16:00:29 +0200

    model weights

--- DINO.GZ21/INFERENCES/ml_models.py
+++ DINO.GZ21/INFERENCES/ml_models.py
@@ -1,8 +1,11 @@
 import numpy as np
 import sys
 import torch
+import pickle
+import sys
+sys.path.append('gz21_ocean_momentum/src/')
 
-from gz21_ocean_momentum.models.fully_conv_net import FullyCNN
+from gz21_ocean_momentum.models.models1 import FullyCNN
 import gz21_ocean_momentum.models.transforms as transforms
 import gz21_ocean_momentum.train.losses as loss_funcs
 
@@ -39,11 +42,8 @@ def momentum_cnn(u, v, mask_u, mask_v):
             u_slice, v_slice = u[:,:,z], v[:,:,z]
             u_slice, v_slice = torch.tensor(u_slice.astype(np.float32)), torch.tensor(v_slice.astype(np.float32))
             inputs = torch.stack([u_slice, v_slice])[None]
-            net = FullyCNN(2, 4, padding='same').eval()
-            transformation = transforms.SoftPlusTransform()
-            transformation.indices = [2, 3] # What to put here ? 
-            net.final_transformation = transformation
-            r = net(inputs)
+            net = model_loading()
+            r = net(inputs) # u, v -> s_x, s_y, std_x, std_y
             Su_mu, Sv_mu, Su_std, Sv_std = r[0, 0], r[0, 1], r[0, 2], r[0, 3]
             u_c = fake_weight * ( u_slice + Su_mu + Su_std*torch.randn_like(Su_std) ).numpy()
             v_c = fake_weight * ( v_slice + Sv_mu + Su_std*torch.randn_like(Sv_std) ).numpy()
@@ -51,7 +51,17 @@ def momentum_cnn(u, v, mask_u, mask_v):
             v[:,:,z] = v_c
         return u*mask_u , v*mask_v
     
-
+@torch.no_grad()
+def model_loading(weights_path='weights/gz21-ocean-momentum/low-resolution/files/', device='cpu') : 
+    net = FullyCNN(padding='same')
+    model_weights = torch.load(weights_path +'trained_model.pth', map_location=device)
+    #net.final_transformation = pickle.load(open(weights_path+'transformation', 'rb')) 
+    transformation = transforms.SoftPlusTransform()
+    transformation.indices = [2, 3] # Careful if model change
+    net.final_transformation = transformation
+    net.load_state_dict(model_weights)
+    net.eval() 
+    return net 
 
 if __name__ == '__main__' : 
     u = np.random.rand(120, 100, 3).astype('float32')
