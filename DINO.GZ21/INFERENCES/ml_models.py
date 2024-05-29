fix forcing fields computation

DINO.GZ21/INFERENCES/ml_models.py
@@ -33,22 +33,25 @@ def Is_None(*inputs):
 # ------------------------------
 @torch.no_grad()
 def momentum_cnn(u, v, mask_u, mask_v):
-    """ Take as input u and v fields and return corrected fields using GZ (2021)  """
+    """ Take as input u and v fields and return subgrid forcing fields using GZ (2021)  """
     if Is_None([u, v]):
         return None
     else:
-        fake_weight = 0.0001
+        alpha = 0.0000001
         for z in range(u.shape[2]):
-            u_slice, v_slice = u[:,:,z], v[:,:,z]
+            normu, normv = np.std(u[:,:,z]), np.std(v[:,:,z])
+            normu = 1.0 if normu == 0.0 else normu
+            normv = 1.0 if normv == 0.0 else normv
+            u_slice, v_slice = u[:,:,z] / normu, v[:,:,z] / normv
             u_slice, v_slice = torch.tensor(u_slice.astype(np.float32)), torch.tensor(v_slice.astype(np.float32))
             inputs = torch.stack([u_slice, v_slice])[None]
             net = model_loading()
             r = net(inputs) # u, v -> s_x, s_y, std_x, std_y
             Su_mu, Sv_mu, Su_std, Sv_std = r[0, 0], r[0, 1], r[0, 2], r[0, 3]
-            u_c = fake_weight * ( u_slice + Su_mu + Su_std*torch.randn_like(Su_std) ).numpy()
-            v_c = fake_weight * ( v_slice + Sv_mu + Su_std*torch.randn_like(Sv_std) ).numpy()
-            u[:,:,z] = u_c
-            v[:,:,z] = v_c
+            fu = ( Su_mu + Su_std*torch.randn_like(Su_std) ).numpy()
+            fv = ( Sv_mu + Su_std*torch.randn_like(Sv_std) ).numpy()
+            u[:,:,z] = fu * alpha
+            v[:,:,z] = fv * alpha
         return u*mask_u , v*mask_v
     
 @torch.no_grad()